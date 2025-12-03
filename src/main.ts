376af6bc633c9470c56ccf536d12bc0d4dff5eba
Adekams <kifademi@gmail.com>
2025-12-03 23:40:57 +0100

    toastr & firestore fix

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,24 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { environment } from './environments/environment';
 import { appRoutes } from 'app/app.routes';
 import { AppComponent } from 'app/app';
+import { provideToastr } from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
     provideHttpClient(),
-    provideAnimationsAsync(),
     provideRouter(appRoutes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
